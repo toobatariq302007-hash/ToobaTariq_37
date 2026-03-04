fc6b5b10220b511ddb7cecad21bb8c235a9d06f7
toobatariq302007-hash <toobatariq302007@gmail.com>
2026-03-04 12:40:16 +0500

    Tooba Tariq first commit

---
+++ car trip.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int fuel, trips;
+
+    cout << "Enter fuel: ";
+    cin >> fuel;
+
+    cout << "Enter number of trips: ";
+    cin >> trips;
+
+    fuel = fuel - (trips * 2);
+
+    cout << "Remaining Fuel: " << fuel;
+
+    return 0;
+}
+
